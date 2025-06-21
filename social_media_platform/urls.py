f032d1aff68a48fe4cecde055b85f5e0ced2348a
Andrew <akolomiec982@gmail.com>
2025-06-21 22:18:05 +0300

    feat: add interactions routing and media files config

--- social_media_platform/urls.py
+++ social_media_platform/urls.py
@@ -23,10 +23,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "api/users/",
-        include("users.urls"),
-    ),
+    path("api/users/", include("users.urls")),
+    path("api/interactions", include("interactions.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
