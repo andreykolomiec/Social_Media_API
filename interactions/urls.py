c60b31ab88c4157046f949ae62947dbd401c3d41
Andrew <akolomiec982@gmail.com>
2025-06-24 19:06:11 +0300

    Register LikeViewSet in interactions URL routing

--- interactions/urls.py
+++ interactions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from interactions.views import FollowViewSet
+from interactions.views import FollowViewSet, LikeViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register("follows", FollowViewSet)
+router.register("likes", LikeViewSet)
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "interactions"
