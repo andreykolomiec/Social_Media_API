3aa4b1210349891da4a86ec9daadf5a7d221376b
Andrew <akolomiec982@gmail.com>
2025-06-24 19:10:54 +0300

    Add posts API routing and OpenAPI documentation with Swagger/ReDoc interfaces

--- social_media_platform/urls.py
+++ social_media_platform/urls.py
@@ -19,12 +19,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
     path("api/interactions", include("interactions.urls")),
+    path("api/posts/", include("posts.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
