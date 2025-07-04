d52847c5f22df91244595512bfcb38e1856e3486
Andrew <akolomiec982@gmail.com>
2025-07-04 23:20:19 +0300

    Update URL routes to support JWT authentication and user self-management endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,31 +1,29 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView,
+
+from users.views import (
+    CreateUserView,
+    LoginUserView,
+    ManageUserView,
+    LogoutView,
+    UserProfileViewSet,
 )
-from users.views import UserRegistrationView, LoginView, LogoutView, UserProfileViewSet
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = DefaultRouter()
 router.register(r"profile", UserProfileViewSet, basename="profile")
 
 urlpatterns = [
-    path("register/", UserRegistrationView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", LoginUserView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("me/", ManageUserView.as_view(), name="me"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("", include(router.urls)),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "schema/swagger/",
-        SpectacularSwaggerView.as_view(url_name="users:schema"),
-        name="swagger",
-    ),
-    path(
-        "schema/redoc/",
-        SpectacularRedocView.as_view(url_name="users:schema"),
-        name="redoc",
-    ),
 ]
 
 app_name = "users"
