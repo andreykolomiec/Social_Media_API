6f5ca39c2dd1a7e513d09ca43624d5a150b9b878
Andrew <akolomiec982@gmail.com>
2025-07-11 23:31:14 +0300

    Register Comment, Like, and Follow models in Django admin

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+from interactions.models import Comment, Like, Follow
+
+
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follow)
