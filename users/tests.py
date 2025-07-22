1bc9da625717a1eda69667e06d09fb6de4276e3a
Andrew <akolomiec982@gmail.com>
2025-07-22 22:09:23 +0300

    added user API tests for registration, authentication, and profile management

--- users/tests.py
+++ users/tests.py
@@ -1,1 +1,66 @@
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        payload = {
+            "email": "test@example.com",
+            "username": "testuser",
+            "password": "testpass123",
+            "password2": "testpass123",
+        }
+        res = self.client.post(reverse("users:register"), payload)
+        print(res.data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload["email"])
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", res.data)
+
+    def test_create_token_for_user(self):
+        user_details = {"email": "test@example.com", "password": "testpass123"}
+        create_user(**user_details)
+        res = self.client.post(reverse("users:login"), user_details)
+        self.assertIn("access", res.data)
+        self.assertIn("refresh", res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_token_invalid_credentials(self):
+        create_user(email="test@example.com", password="goodpass")
+        payload = {"email": "test@example.com", "password": "badpass"}
+        res = self.client.post(reverse("users:login"), payload)
+        self.assertNotIn("access", res.data)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    def setUp(self):
+        self.user = create_user(email="user@example.com", password="testpass123")
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        res = self.client.get(reverse("users:me"))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["email"], self.user.email)
+
+    def test_post_me_not_allowed(self):
+        res = self.client.post(reverse("users:me"), {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        payload = {"password": "newpass456"}
+        res = self.client.patch(reverse("users:me"), payload)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password(payload["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
